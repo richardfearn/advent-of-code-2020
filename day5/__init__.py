e41f17b210a3645aaa419ccee19a7b681b805fed
Richard Fearn <richardfearn@gmail.com>
2020-12-05 10:47:20 +0000

    Day 5 - convert boarding pass directly to seat ID

--- day5/__init__.py
+++ day5/__init__.py
@@ -4,22 +4,20 @@ def seat_number(boarding_pass):
     return row, col
 
 
-def seat_id(seat):
-    return seat[0] * 8 + seat[1]
+def seat_id(boarding_pass):
+    return int(boarding_pass.replace("F", "0").replace("B", "1").replace("L", "0").replace("R", "1"), 2)
 
 
 def part_1(boarding_passes):
     boarding_passes = boarding_passes.split()
-    seat_numbers = [seat_number(bp) for bp in boarding_passes]
-    seat_ids = [seat_id(seat) for seat in seat_numbers]
+    seat_ids = [seat_id(bp) for bp in boarding_passes]
     return max(seat_ids)
 
 
 def part_2(boarding_passes):
 
     boarding_passes = boarding_passes.split()
-    seat_numbers = [seat_number(bp) for bp in boarding_passes]
-    seat_ids = set(seat_id(seat) for seat in seat_numbers)
+    seat_ids = set(seat_id(bp) for bp in boarding_passes)
 
     expected_sum = sum_up_to(max(seat_ids)) - sum_up_to(min(seat_ids) - 1)
     missing_seat_id = expected_sum - sum(seat_ids)
--- day5/tests.py
+++ day5/tests.py
@@ -14,18 +14,18 @@ class Part1Tests(unittest.TestCase):
 
     def test_first_example(self):
         self.assertEqual(day5.seat_number(EXAMPLE_1), (44, 5))
-        self.assertEqual(day5.seat_id((44, 5)), 357)
+        self.assertEqual(day5.seat_id(EXAMPLE_1), 357)
 
     def test_other_examples(self):
 
         self.assertEqual(day5.seat_number(EXAMPLE_2), (70, 7))
-        self.assertEqual(day5.seat_id(day5.seat_number(EXAMPLE_2)), 567)
+        self.assertEqual(day5.seat_id(EXAMPLE_2), 567)
 
         self.assertEqual(day5.seat_number(EXAMPLE_3), (14, 7))
-        self.assertEqual(day5.seat_id(day5.seat_number(EXAMPLE_3)), 119)
+        self.assertEqual(day5.seat_id(EXAMPLE_3), 119)
 
         self.assertEqual(day5.seat_number(EXAMPLE_4), (102, 4))
-        self.assertEqual(day5.seat_id(day5.seat_number(EXAMPLE_4)), 820)
+        self.assertEqual(day5.seat_id(EXAMPLE_4), 820)
 
     def test_with_input(self):
         boarding_passes = utils.read_input(5)
