a9eea47f504af8a73ca44d9345d171016649cfa0
Richard Fearn <richardfearn@gmail.com>
2020-12-09 09:58:57 +0000

    Day 8 - better solution to part 2

--- day8/__init__.py
+++ day8/__init__.py
@@ -23,9 +23,9 @@ def part_1(instructions):
     return execute(instructions)[1]
 
 
-def execute(instructions):
+def execute(instructions, start=0):
 
-    pc = 0
+    pc = start
     acc = 0
 
     executed = set()
@@ -54,17 +54,101 @@ def execute(instructions):
 
 
 def part_2(instructions):
-
     instructions = parse(instructions)
+    subprograms = find_subprograms(instructions)
+    will_terminate = find_instructions_that_will_terminate(instructions, subprograms)
+    return find_instruction_to_change(instructions, will_terminate)
 
-    for i, (op, arg) in enumerate(instructions):
 
-        if op in (NOP, JMP):
+def find_subprograms(instructions):
 
-            instructions[i][0] = JMP if (op == NOP) else NOP
+    # Makes it easier to find connected components
+    graph = undirected_graph(instructions)
 
-            result, acc = execute(instructions)
-            if result == TERMINATED:
-                return acc
+    components = []
+    search_start_remaining = set(range(len(instructions)))
 
-            instructions[i][0] = op
+    while len(search_start_remaining) > 0:
+
+        search_start = search_start_remaining.pop()
+        visited = set()
+        to_explore = [search_start]
+
+        while len(to_explore) > 0:
+            next_node = to_explore.pop(0)
+            if next_node not in visited:
+                if next_node in search_start_remaining:
+                    search_start_remaining.remove(next_node)
+                visited.add(next_node)
+                to_explore += graph[next_node]
+
+        components.append(visited)
+
+    return components
+
+
+def undirected_graph(instructions):
+
+    graph = {pc: [] for pc in range(len(instructions))}
+
+    for pc, (op, arg) in enumerate(instructions):
+
+        next_pc = (pc + arg) if (op == JMP) else (pc + 1)
+
+        if next_pc < len(instructions):
+            graph[pc].append(next_pc)
+            graph[next_pc].append(pc)
+
+    return graph
+
+
+def find_instructions_that_will_terminate(instructions, subprograms):
+
+    will_terminate = set()
+
+    for s in subprograms:
+        start_instruction = list(s)[0]
+        if execute(instructions, start_instruction)[0] == TERMINATED:
+            will_terminate.update(s)
+
+    return will_terminate
+
+
+def find_instruction_to_change(instructions, will_terminate):
+
+    pc = 0
+    acc = 0
+
+    executed = set()
+
+    instruction_changed = False
+
+    while True:
+
+        if pc > len(instructions) - 1:
+            return acc
+
+        if pc in executed:
+            raise Exception("infinite loop")
+
+        op, arg = instructions[pc]
+
+        executed.add(pc)
+
+        if (not instruction_changed) and op in (NOP, JMP):
+
+            next_pc_with_change = (pc + arg) if (op == NOP) else (pc + 1)
+
+            if (next_pc_with_change > len(instructions) - 1) or (next_pc_with_change in will_terminate):
+                op = JMP if (op == NOP) else NOP
+                instruction_changed = True
+
+        if op == NOP:
+            pc += 1
+
+        elif op == ACC:
+            acc += arg
+            pc += 1
+
+        elif op == JMP:
+            pc += arg
